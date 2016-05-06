5ed700acc70470dc95fd145ed0723ceec0ad5539
Khoa Nguyen <minhkhoa4783@gmail.com>
2016-05-06 15:48:14 -0700

    I'M DONE

---
+++ app/controllers/dogs_controller.rb
@@ -0,0 +1,47 @@
+class DogsController < ApplicationController
+  before_action :set_dog, only: [:show, :edit, :update, :destroy]
+
+  # GET /dogs
+  # GET /dogs.json
+  def index
+    @dogs = Dog.all
+  end
+
+  # GET /dogs/1
+  # GET /dogs/1.json
+  def show
+  end
+
+  # GET /dogs/new
+  def new
+    @dog = Dog.new
+  end
+
+
+  # POST /dogs
+  # POST /dogs.json
+  def create
+    @dog = Dog.new(dog_params)
+
+    respond_to do |format|
+      if @dog.save
+        format.html { redirect_to @dog, notice: 'Dog was successfully created.' }
+        format.json { render :show, status: :created, location: @dog }
+      else
+        format.html { render :new }
+        format.json { render json: @dog.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_dog
+      @dog = Dog.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def dog_params
+      params.require(:dog).permit(:name, :age)
+    end
+end
